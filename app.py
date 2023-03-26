ce0441640e90b6032fe896a1363a7d9da04c2da6
Aman Onkar Onkar <>
2023-03-26 14:12:37 +0000

    add Flask in it

---
+++ app.py
@@ -0,0 +1,13 @@
+# print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+# print(__name__)
+@app.route("/")
+def hello_world():
+  return "Hello Detective"
+
+# print(__name__)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',debug=True)
+  # print("Inside the if now.")
